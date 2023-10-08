39e03dd37ccafafce83501362dfd3ace35cc6b20
JYOTIRANJAN MOHANTY <107472728+jyotiranjan1997@users.noreply.github.com>
2023-10-08 08:22:40 -0700

    Add files via upload

--- src/Routes/MemberRoute.js
+++ src/Routes/MemberRoute.js
@@ -19,6 +19,6 @@ MemberRoute.post("/", Create_Member_Controller); // Create Contact details Route
 MemberRoute.get("/", verifyadmin, Find_Member_Controller); // Find contacts Data with admin authorization
 MemberRoute.put("/", verifyMember, Update_Member_Controller); // Find single Data with admin authorization
 MemberRoute.get("/:id", verifyadmin, verifyMember, Single_Member_Controller); // update Data with admin authorization
-MemberRoute.put("/change/password", MemberPasswordChange);
+MemberRoute.put("/change/password", verifyadmin, MemberPasswordChange);
 MemberRoute.delete("/:id", verifyadmin, Delete_Member_Controller);
 module.exports = { MemberRoute };
