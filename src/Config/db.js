1ee9e5adf8874224ff89ec034cd6ad7c5a2199c8
JYOTIRANJAN MOHANTY <107472728+jyotiranjan1997@users.noreply.github.com>
2023-10-08 07:39:27 -0700

    Add files via upload

--- src/Config/db.js
+++ src/Config/db.js
@@ -3,10 +3,7 @@ mongoose.set("strictQuery", false);
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL, {
-      useNewUrlParser: true,
-      dbName: "swag-shop",
-    });
+    await mongoose.connect(process.env.DB_URL);
     console.log("DB Connection Success...");
   } catch (ex) {
     console.log("DB connection Failed...");
